ea2ab4357dbf60d019163b3feeb2b33999703d59
BestSemper <futurekim07@gmail.com>
2026-01-05 12:43:25 +0900

    added vercel analytics
    Signed-off-by: BestSemper <futurekim07@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const inter = Inter({
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className={`${inter.variable} antialiased`}>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
